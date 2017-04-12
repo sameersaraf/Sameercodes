0393ba9da1105b6f885a7245fc7af1abc2532c4b
sameer saraf <sameersaraf33@gmail.com>
2017-04-12 19:03:01 +0530

    Bin2Dec

---
+++ Text palandrome/Text palandrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_palandrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string s, revs = "";
+                Console.WriteLine(" Enter string");
+                s = Console.ReadLine();
+                for (int i = s.Length - 1; i >= 0; i--) 
+                {
+                    revs += s[i].ToString();
+                }
+                if (revs == s)
+                {
+                    Console.WriteLine("String is Palindrome");
+                }
+                else
+                {
+                    Console.WriteLine("String is not Palindrome");
+                }
+                //Console.ReadKey();
+            }
+        }
+    }
+}
