0da7307e0dc96f4de60e52544db131cc1e05a7e9
sameer saraf <sameersaraf33@gmail.com>
2017-04-12 16:40:03 +0530

    inputs syntax

---
+++ Z pattern/Z pattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+
+
+            for (row = 0; row <= 6; row++)
+            {
+                for (column = 0; column <= 6; column++)
+                {
+                    if (((row == 0 || row == 6) && column >= 0 && column <= 6) || row + column == 6 )
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+    }
+}
+    
+
---
+++ input practice/input practice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            float num2;
+            //string char1;
+            double Doo;
+            Console.WriteLine("int");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("flot");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("string");
+            string char1 = Console.ReadLine();
+            Console.WriteLine("double");
+            Doo = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(Doo);
+            Console.WriteLine(char1);
+
+        }
+    }
+}
