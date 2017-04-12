411e15899cf6abaaef2e8a6f0b2dd5e2fbe7028b
sameer saraf <sameersaraf33@gmail.com>
2017-04-12 18:36:10 +0530

    rev string

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str, reversedstring = " ";
+            int Length;
+            Console.Write("Enter A String : ");
+            Str = Console.ReadLine();
+            Length = Str.Length - 1;
+            while (Length >= 0)
+            {
+                reversedstring = reversedstring + Str[Length];
+                Length--;
+            }
+            Console.WriteLine("Reverse  String  Is  {0}", reversedstring);
+          //  Console.ReadLine();
+        }
+    }
+}
+    
---
+++ Vobles check/Vobles check/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vobles_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str;
+            Str = Console.ReadLine();
+           // Console.WriteLine(Str);
+            int total = 0;
+
+            Console.WriteLine("Enter a Sentence");
+           
+
+            for (int i = 0; i < Str.Length; i++)
+            {
+                if (Str.Contains("a") || Str.Contains("e") || Str.Contains("i") || Str.Contains("o") || Str.Contains("u"))
+                {
+                    total++;
+                }
+                
+            }
+            Console.WriteLine("Your total number of vowels is: {0}", total);
+            Console.WriteLine("Your total number of vowels is: {0}", total);
+
+
+            Console.ReadLine();
+        }
+    }
+}
