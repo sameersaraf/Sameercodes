f8a2a49d8df0249b8b4da247f8f7c7449ae77a24
hp <hp@SAMEER-SARAF>
2017-04-12 12:43:57 +0530

    Add project files.

---
+++ PiFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0.0;
+            int maxDenominator = 10000000;
+            for (int denominator = 1; denominator <= maxDenominator; denominator += 2)
+            {  
+                if (denominator % 4 == 1)
+                {
+                    sum += 1;
+                }
+                else if (denominator % 4 == 3)
+                {
+                    sum -= 1;
+                }
+                else
+                {   // remainder of 0 or 2
+                    Console.WriteLine("The computer has gone crazy?!");
+                }
+            }
+        }
+    }
+}
