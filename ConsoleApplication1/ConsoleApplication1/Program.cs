35574d240af94f49fbb58d92b6efa0fb9aee679c
sameer saraf <sameersaraf33@gmail.com>
2017-04-12 14:26:10 +0530

    Square board

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ExtractDigit
+    {
+        static void Main(string[] args)
+        {
+            int n=1534785;
+            while(n>0)
+            {
+                int Temp = n % 10;
+                Console.Write("  ");
+                Console.Write(Temp);
+                n = n / 10;
+
+            }
+        }
+    }
+}
---
+++ SquareBoard/SquareBoard/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            for(int row=1;row<=size;++row)
+            {
+                for(int col=1;col<=size;++col)
+                {
+                    Console.Write(" #");
+
+                }
+                Console.WriteLine(" ");
+
+            }
+
+        }
+    }
+}
