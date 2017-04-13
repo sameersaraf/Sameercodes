75abaa8ca46347438dc651aadef10ceb9534b78f
sameer saraf <sameersaraf33@gmail.com>
2017-04-13 10:45:53 +0530

    Magic sum not completed yet

---
+++ MagicSum/MagicSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int i = Int32.Parse(Console.ReadLine());
+            string[] answer = new string[10];
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = Console.ReadLine();
+                if(answer==8)
+                {
+                    answer = answer + i;
+
+                }
+               
+            }
+           // answer[i] = Console.WriteLine();
+        }
+    }
+}
---
+++ Oct2Dec/Oct2Dec/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oct2Dec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decimalNumber, quotient, i = 1, j;
+            int[] octalNumber = new int[100];
+            Console.WriteLine("Enter a Decimal Number :");
+            decimalNumber = int.Parse(Console.ReadLine());
+            quotient = decimalNumber;
+            while (quotient != 0)
+            {
+                octalNumber[i++] = quotient % 8;
+                quotient = quotient / 8;
+            }
+            Console.Write("Equivalent Octal Number is ");
+            for (j = i - 1; j > 0; j--)
+                Console.Write(octalNumber[j]);
+            Console.Read();
+        }
+    }
+}
