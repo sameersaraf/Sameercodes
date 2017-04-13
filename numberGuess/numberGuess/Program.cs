9d3891010dc123fdade42dc4b92f3010b2bf0d70
sameer saraf <sameersaraf33@gmail.com>
2017-04-13 13:16:57 +0530

    number guessing game

---
+++ numberGuess/numberGuess/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToGuess = 54;
+            int userGuess = 0;
+
+            while (userGuess != numberToGuess)
+            {
+                Console.Write("Enter your guess: ");
+                int.TryParse(Console.ReadLine(), out userGuess);
+
+                if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("{0} is too high!", userGuess);
+                }
+                else if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("{0} is too low!", userGuess);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is right! Congratulations.", userGuess);
+                }
+            }
+        }
+    }
+    }
+
